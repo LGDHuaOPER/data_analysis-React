a06f2debc764e0080ce46cfb5b45407752af2f72
LGDHuaOPER <861635051@qq.com>
2019-01-08 21:43:08 +0800

    取消entryBuild devBuild文件夹加入版本库

--- entryBuild/index.js
+++
@@ -1,7 +0,0 @@
-import 'babel-polyfill';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Index from '../app/component/index/Index.jsx';
-import MyLayout from '../app/component/common/MyLayout';
-ReactDOM.render([<MyLayout key="MyLayout"/>], document.getElementById('app'));
-ReactDOM.render(<Index key="Index"/>, document.getElementById('proj-content'));
